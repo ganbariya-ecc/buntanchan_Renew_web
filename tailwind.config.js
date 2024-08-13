a6439f65031221309b1d3a9a70bdf2874571f8e2
haruna1256 <haru_kawagishi@icloud.com>
2024-08-14 01:51:07 +0900

    add code 移動

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./public/**/*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      // フォントファミリー設定
+    fontFamily: {
+      'coment': ['"Kiwi Maru"', ...defaultTheme.fontFamily.sans],
+      'title': ['"Yusei Magic"', ...defaultTheme.fontFamily.sans],
+    },
+    // 色設定
+    colors: {
+      'main-color': 'rgba(246, 219, 186, 0.5)',
+      'sub-color': 'rgba(255 247 237)',
+      'green': '#4AB4A1',
+    },
+    },
   },
   plugins: [],
 };

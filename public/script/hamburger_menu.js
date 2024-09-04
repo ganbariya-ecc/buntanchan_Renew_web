d2769a2ce5a697af00f75c798970f17d103937eb
haruna1256 <haru_kawagishi@icloud.com>
2024-09-05 05:56:24 +0900

    jquery の追加

--- public/script/hamburger_menu.js
+++ public/script/hamburger_menu.js
@@ -1,8 +1,8 @@
 // ハンバーガーメニューのjs
-$(function () {
-    $('.js-btn').on('click', function () { // js-btnクラスをクリックすると、
-        $('.menu').toggleClass('open'); // メニューにopenクラスをつけ外しする
-        $('.btn-line').toggleClass('open'); // バーガーの線にopenクラスをつけ外しする
-        $('.overlay').toggleClass('show'); // グレイレイヤーにshowクラスをつけ外しする
+$(document).ready(function () {
+    $('.js-btn').on('click', function () { // js-btnクラスがクリックされたときに実行
+        $('.menu').toggleClass('open'); // メニューにopenクラスを追加・削除
+        $('.btn-line').toggleClass('open'); // バーガーの線にopenクラスを追加・削除
+        $('.overlay').toggleClass('show'); // グレイレイヤーにshowクラスを追加・削除
     });
 });
